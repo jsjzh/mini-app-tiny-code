540769754d31d1b6a1bc88aaddde766df22ae99f
jsjzh <kimimi_king@163.com>
2018-12-25 20:58:59 +0800

    continue

--- src/type/functional_example.js
+++ src/type/functional_example.js
@@ -1,8 +1,8 @@
-import * as R from "ramda";
-import $ from "jquery";
+import * as R from "ramda"
+import $ from "jquery"
 
 $(document).ready(function() {
-  const { curry, compose } = R;
+  const { curry, compose } = R
 
   const CARS = [
     { name: "Ferrari FF", horsepower: 660, dollar_value: 700000, in_stock: true },
@@ -11,35 +11,36 @@ $(document).ready(function() {
     { name: "Audi R8", horsepower: 525, dollar_value: 114200, in_stock: false },
     { name: "Aston Martin One-77", horsepower: 750, dollar_value: 1850000, in_stock: true },
     { name: "Pagani Huayra", horsepower: 700, dollar_value: 1300000, in_stock: false }
-  ];
+  ]
 
   const trace = curry(function(tag, x) {
-    console.log(tag, x);
-    return x;
-  });
+    console.log(tag, x)
+    return x
+  })
 
   let Impure = {
     getJSON: curry(function(callback, url) {
       $.getJSON(url, callback)
     }),
     setHtml: curry(function(sel, html) {
-      $(sel).html(html);
+      $(sel).html(html)
     })
   }
 
   let url = function(target) { return "/static/data/cat.json?target=" + target }
 
-  let img = function(url) {
-    return $('<img />', { src: url });
-  };
+  let img = function(url) { return $('<img />', { src: url }) }
 
-  let imageUrl = compose(R.prop("thumbURL"));
-  let filterSrcs = curry(function(x) { return !!x });
-  let srcs = compose(R.filter(filterSrcs), R.map(imageUrl), R.prop("data"));
+  let notEmpty = function(x) { return x != null }
 
-  let renderImages = compose(Impure.setHtml("#container"), trace("after srcs"), srcs);
-  let app = compose(Impure.getJSON(renderImages), url);
+  let render = compose(Impure.setHtml("#container"))
 
-  app("cats");
+  let targets = compose(render, R.map(img))
 
-});
+  let srcs = compose(targets, R.filter(notEmpty), R.map(R.prop("thumbURL")), R.prop("data"))
+
+  let app = compose(Impure.getJSON(srcs), url)
+
+  app("cats")
+
+})
