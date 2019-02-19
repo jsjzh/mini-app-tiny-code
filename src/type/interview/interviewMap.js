cf2da7e376c9c5d2af955738c1fb4ef347a1a437
jsjzh <kimimi_king@163.com>
2019-02-19 17:55:27 +0800

    add

--- src/type/interview/interviewMap.js
+++ src/type/interview/interviewMap.js
@@ -194,7 +194,7 @@
 //   console.log(clone)
 // })()
 
-// 模块化
+// 模块化 ES6 目前不支持，但已有提案
 // 在有 babel 的情况下，可以直接使用 ES6 的 export 和 import
 // // file a.js
 // export function a() {}
@@ -204,3 +204,31 @@
 // // file c.js
 // import { a, b } from 'a.js'
 // import XXX from 'b.js'
+// 模块化 CommonJS 支持动态导入 require(${path}/xx.js)
+// // file a.js
+// module.exports = { a: 1 }
+// exports.b = 2
+// let foo = require('a.js')
+// foo.a // 1
+// foo.b // 2
+// 两个模块化的区别
+// 前者支持动态导入，也就是 require(${path}/xx.js)，后者目前不支持，但是已有提案
+// 前者是同步导入，因为用于服务端，文件都在本地，同步导入即使卡住主线程影响也不大。而后者是异步导入，因为用于浏览器，需要下载文件，如果也采用同步导入会对渲染有很大影响
+// 前者在导出时都是值拷贝，就算导出的值变了，导入的值也不会改变，所以如果想更新值，必须重新导入一次。但是后者采用实时绑定的方式，导入导出的值都指向同一个内存地址，所以导入值会跟随导出值变化
+// 后者会编译成 require/exports 来执行的
+
+// 防抖
+function debounce(func, wait = 50) {
+  let timer = 0
+  return function() {
+    if (timer) clearTimeout(timer)
+    timer = setTimeout(() => {
+      func.apply(this, arguments)
+    }, wait)
+  }
+}
+foo = debounce(function() {
+  console.log(123)
+}, 100)
+foo()
+foo()
