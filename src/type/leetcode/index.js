83c143cbad94661272454d0082c039fb110ddd7e
jsjzh <kimimi_king@163.com>
2019-03-08 10:01:58 +0800

    添加三个这两天做的简单级别的算法题，有两题需要优化算法

--- src/type/leetcode/index.js
+++ src/type/leetcode/index.js
@@ -1,4 +1,13 @@
-var twoSum = function(nums, target) {
+/**
+ * CLEAR
+ * 给出一个目标数字，查询数组中是否有和为目标的两个数字
+ * @param {Array} nums 待查询数组
+ * @param {Number} target 目标数字
+ *
+ * 官方解题思路
+ * ...
+ */
+let twoSum = function(nums, target) {
   for (let index = 0; index < nums.length; index++) {
     const number = nums[index]
     let other = target - number
@@ -12,18 +21,45 @@ var twoSum = function(nums, target) {
   return []
 }
 
-var reverse = function(num) {
-  if (num < -Math.pow(2, 31) || num > Math.pow(2, 31) - 1 || num === 0) return 0
+/**
+ * TODO 需要完善
+ * 将一个数字反转，若反转后超出 [-2^31, 2^31-1] 范围，则返回 0
+ * @param {Number} x 数字
+ *
+ * 官方解题思路
+ * 比较神奇的一个想法，出栈入栈，但因为没有辅助函数，所以可以用 num % 10 来实现
+ */
+let reverse = function(x) {
+  if (x === 0) return 0
   let flag = false
-  if (num > 0) flag = true
-  num = Math.abs(num)
+  if (x > 0) flag = true
+  x = Math.abs(x)
     .toString()
     .split('')
     .reverse()
     .join('')
-  if (flag) num = +num
-  else num = -+num
+  if (flag) x = +x
+  else x = -+x
 
-  if (num < -Math.pow(2, 31) || num > Math.pow(2, 31) - 1) return 0
-  else return num
+  if (x < -Math.pow(2, 31) || x > Math.pow(2, 31) - 1) return 0
+  else return x
+}
+
+/**
+ * TODO 需要完善
+ * 判断一个整数是否是回文数
+ * @param {Number} x 数字
+ *
+ * 官方解题思路
+ * 可以判断数字的前一半和后一半是否相同
+ */
+let isPalindrome = function(x) {
+  if (x < 0) return false
+  if (x === 0) return true
+  let y = +x
+    .toString()
+    .split('')
+    .reverse()
+    .join('')
+  return y === x
 }
