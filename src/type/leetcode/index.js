1dc95afdae85a2e673a193ae5823eeae86c440a8
jsjzh <kimimi_king@163.com>
2019-03-13 09:56:35 +0800

    添加一个新的题目，判断括号字符串是否有效

--- src/type/leetcode/index.js
+++ src/type/leetcode/index.js
@@ -3,7 +3,7 @@
  * @Email: kimimi_king@163.com
  * @LastEditors: jsjzh
  * @Date: 2019-03-08 09:45:09
- * @LastEditTime: 2019-03-13 09:49:08
+ * @LastEditTime: 2019-03-13 09:54:57
  * @Description
  *  果然每天的生活都需要点算法题调剂调剂，每天都是重复的业务代码太无趣了，我渴望一点需要动脑子的东西，遂就有了这个小项目
  *  写上来的代码都是可以通过 leedcode 的测试的，只不过嘛，用时和内存消耗就没有那么完美了，但我会对不满意的题目重写一遍，开拓新的思路，撒花
@@ -123,3 +123,20 @@ let longestCommonPrefix = function(strs) {
   }
   return rtStr
 }
+
+/**
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效
+ * 有效字符串需满足：
+ *  左括号必须用相同类型的右括号闭合
+ *  左括号必须以正确的顺序闭合
+ *  注意空字符串可被认为是有效字符串
+ * @param {String} s
+ * @return {Boolean}
+ */
+let isValid = function(s) {}
+
+isValid('()')
+isValid('()[]{}')
+isValid('(]')
+isValid('([)]')
+isValid('{[]}')
