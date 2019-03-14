4a903511287dc022d53ef10e4e1a5fb1bc5632ae
jsjzh <kimimi_king@163.com>
2019-03-14 09:41:12 +0800

    今天只做了一道算法题，不过第二道也有进展，单向链表算是搞清楚了

--- src/type/leetcode/index.js
+++ src/type/leetcode/index.js
@@ -3,7 +3,7 @@
  * @Email: kimimi_king@163.com
  * @LastEditors: jsjzh
  * @Date: 2019-03-08 09:45:09
- * @LastEditTime: 2019-03-13 09:54:57
+ * @LastEditTime: 2019-03-14 09:40:34
  * @Description
  *  果然每天的生活都需要点算法题调剂调剂，每天都是重复的业务代码太无趣了，我渴望一点需要动脑子的东西，遂就有了这个小项目
  *  写上来的代码都是可以通过 leedcode 的测试的，只不过嘛，用时和内存消耗就没有那么完美了，但我会对不满意的题目重写一遍，开拓新的思路，撒花
@@ -78,6 +78,7 @@ let isPalindrome = function(x) {
 
 const roman = { CM: 900, CD: 400, XC: 90, XL: 40, IX: 9, IV: 4, M: 1000, D: 500, C: 100, L: 50, X: 10, V: 5, I: 1 }
 /**
+ * CLEAR
  * 罗马数字转整数
  * 给定一个罗马数字，将其转换成整数，输入确保在 1 至 3999 的范围内
  * @param {String} s
@@ -101,6 +102,7 @@ let romanToInt = function(s) {
 }
 
 /**
+ * CLEAR
  * 查找字符串数组中的最长公共前缀
  * 如果不存在公共前缀则返回空字符串 ""
  * 所有输入只包含小写字母 a-z
@@ -125,6 +127,7 @@ let longestCommonPrefix = function(strs) {
 }
 
 /**
+ * CLEAR
  * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效
  * 有效字符串需满足：
  *  左括号必须用相同类型的右括号闭合
@@ -133,10 +136,46 @@ let longestCommonPrefix = function(strs) {
  * @param {String} s
  * @return {Boolean}
  */
-let isValid = function(s) {}
+let isValid = function(s) {
+  if (s.length & 1) return false
+  let old
+  do {
+    old = s
+    s = s.replace(/(\[\]|\(\)|\{\})/g, '')
+  } while (old.length !== s.length)
+  return s.length === 0
+}
 
-isValid('()')
-isValid('()[]{}')
-isValid('(]')
-isValid('([)]')
-isValid('{[]}')
+/**
+ * TODO 待完成
+ * 将两个有序链表合并为一个新的有序链表并返回y
+ * 新链表是通过拼接给定的两个链表的所有节点组成的
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+/**
+ * Definition for singly-linked list.
+ */
+
+function ListNode(val) {
+  this.val = val
+  this.next = null
+}
+
+ListNode.prototype.push = function(num) {
+  let newNode = new ListNode(num)
+  let currNode = this
+  if (currNode.next) currNode = currNode.next
+  currNode.next = newNode
+  return this
+}
+
+let mergeTwoLists = function(l1, l2) {
+  console.log(arguments)
+}
+
+let L = new ListNode(1).push(2).push(4)
+let R = new ListNode(1).push(3).push(4)
+
+mergeTwoLists(L, R)
